e45ef51483caf39c8af4e50a4b1d7852b1a21df9
Rushi Patel <rushipatel2589@gmail.com>
2022-09-11 18:27:19 -0400

    Commit to fix tailwind intellisense

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
+      {/*Navigation Bar*/}
+      <nav className='w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center'>
+        <div className='w-full md:w-880 bg-navBar'></div>
+      </nav>
+
+      <div className="relative" id="home">
+        <Spline scene="https://prod.spline.design/BAM4-3qugMZFaT2a/scene.splinecode" />
+      </div>
     </div>
   );
 }
